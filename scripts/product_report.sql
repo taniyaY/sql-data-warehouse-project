db0b03da0fdbdf12bef789a59ebec97eda493c89
taniyaY <taniyakamboj0105@gmail.com>
2025-11-09 17:47:29 +0200

    Update and rename customer_project.sql to product_report.sql

--- scripts/product_report.sql
+++ scripts/product_report.sql
@@ -1,23 +1,23 @@
 /*
 ===============================================================================
-Customer Report
+Product Report
 ===============================================================================
 Purpose:
-    - This report consolidates key customer metrics and behaviors
+    - This report consolidates key product metrics and behaviors.
 
 Highlights:
-    1. Gathers essential fields such as names, ages, and transaction details.
-	2. Segments customers into categories (VIP, Regular, New) and age groups.
-    3. Aggregates customer-level metrics:
-	   - total orders
-	   - total sales
-	   - total quantity purchased
-	   - total products
-	   - lifespan (in months)
+    1. Gathers essential fields such as product name, category, subcategory, and cost.
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
+    3. Aggregates product-level metrics:
+       - total orders
+       - total sales
+       - total quantity sold
+       - total customers (unique)
+       - lifespan (in months)
     4. Calculates valuable KPIs:
-	    - recency (months since last order)
-		- average order value
-		- average monthly spend
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
 ===============================================================================
 */
 CREATE VIEW gold.product_reports AS
